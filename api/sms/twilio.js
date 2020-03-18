cc720c26a74a62039d511c7c1e48ac12a430960b
Gudiel Cobon <gudielcobon@gmail.com>
2020-03-18 14:58:36 -0600

    resolving vulnerability

--- api/sms/twilio.js
+++ api/sms/twilio.js
@@ -1,15 +1,15 @@
 'use strict'
 
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
 
-const accountSid = ''; 
-const authToken = ''; 
 const client = require('twilio')(accountSid, authToken);
 
 client.messages
   .create({
      body: 'cuerpo del sms',
-     from: '+',
-     to: '+'
+     from: '+12036543425',
+     to: '+50241186849'
    })
   .then(message => {
             console.log(message.sid);

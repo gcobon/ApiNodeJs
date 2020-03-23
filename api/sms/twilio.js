c973c8db32a1b5c6ea38d3704b6cc3825a3df665
Gudiel Cobon <gudielcobon@gmail.com>
2020-03-22 18:54:49 -0600

    UPDATE VULNERABILITY

--- api/sms/twilio.js
+++ api/sms/twilio.js
@@ -2,22 +2,21 @@
 
 require('dotenv').config()
 
-console.log('sid: ', process.env.TWILIO_ACCOUNT_SID);
+console.log('sid: ', );
 console.log('PORT: ', process.env.PORT);
 
-/*
-const accountSid = 'AC55efc0483c64c3a815ccfb2a100814aa';
-const authToken = 'c05469d048383e8de67804c89ead7803';
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
 
 const client = require('twilio')(accountSid, authToken);
 
 client.messages
   .create({
     body: "All in the game, yo",
-    from: "+12036543425",
-    to: "+50241186849"
+    from: "+",
+    to: "+"
   })
   .then(message => {
     console.log(message.sid);
   });
-*/

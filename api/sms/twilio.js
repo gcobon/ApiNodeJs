2ad12782a79a9e1c0370ae40c50fdba8130dbd13
Gudiel Cobon <gudielcobon@gmail.com>
2020-03-22 18:45:10 -0600

    update changes

--- api/sms/twilio.js
+++ api/sms/twilio.js
@@ -1,16 +1,23 @@
-'use strict'
+"use strict";
 
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
+require('dotenv').config()
+
+console.log('sid: ', process.env.TWILIO_ACCOUNT_SID);
+console.log('PORT: ', process.env.PORT);
+
+/*
+const accountSid = 'AC55efc0483c64c3a815ccfb2a100814aa';
+const authToken = 'c05469d048383e8de67804c89ead7803';
 
 const client = require('twilio')(accountSid, authToken);
 
 client.messages
   .create({
-     body: 'cuerpo del sms',
-     from: '+12036543425',
-     to: '+50241186849'
-   })
+    body: "All in the game, yo",
+    from: "+12036543425",
+    to: "+50241186849"
+  })
   .then(message => {
-            console.log(message.sid);
+    console.log(message.sid);
   });
+*/
